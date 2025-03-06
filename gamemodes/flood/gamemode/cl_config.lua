5c70bc629d12da9215e570fa23252da951ca873d
russianpoop228 <kozyrev.georgiy.2016@mail.ru>
2025-03-07 02:39:16 +0300

    [Revert] CFC Style

--- gamemodes/flood/gamemode/cl_config.lua
+++ gamemodes/flood/gamemode/cl_config.lua
@@ -1,126 +1,104 @@
---[[
-
-Floattranslation
-
-]]
-
-local floaty = {}
-
-floaty["-3"] = "Extremely Bad"
-floaty["-2"] = "Very Bad"
-floaty["-1"] = "Bad"
-floaty["0"]  = "Average"
-floaty["1"]  = "Good"
-
-function TranslateFloat(float)
-	return floaty[tostring(float)]
-end
-
---[[
-
-Weapon left and rightclick underwater allowance
-
-]]
-
-AllowLeftClick = {
-
-	["weapon_crowbarfm"]  = true,
-	["weapon_sledgehammer"]  = true,
-	["weapon_pistolfm"]  = true,
-	["weapon_stunstick"]  = true,
-	["weapon_harpoonfm"]  = true,
-	["weapon_crossbow"]   = true,
-	["weapon_physgun"]    = true,
-	["weapon_physcannon"] = true,
-	["gmod_tool"]         = true,
-	["weapon_crowbar"]    = true,
-
-}
-
-AllowRightClick = {
-
-	["weapon_harpoonfm"]   = true,
-	["weapon_physgun"]     = true,
-	["weapon_sledgehammer"]  = true,
-	["weapon_physcannon"]  = true,
-	["gmod_tool"]          = true,
-	["weapon_awpfm"]       = true,
-	["weapon_svufm"]       = true,
-	["weapon_ak47fm"]      = true,
-	["weapon_m4fm"]        = true,
-	["weapon_tmpfm"]       = true,
-	["weapon_galilfm"]     = true,
-	["weapon_parafm"]      = true,
-	["weapon_fivesevenfm"] = true,
-	["weapon_deaglefm"]    = true,
-	["weapon_crossbow"]    = true,
-	["weapon_famasfm"]     = true,
-
-}
-
---[[
-
-Festives
-
-]]
-
-Gift_PopupTitle = "Yay!"
-Gift_PopupMessage = "You've received a gift. You were given:"
-
---[[
-
-Icon adjusting
-
-]]
-
-IconCoords = {
-
-	--Store content
-
-	["models/mymodels/fezhat.mdl"]                 = {Vector(0,0,0)   , Angle(0,-40,0)   , 20    , Color(255,0,0)},
-	["models/daftpunk/ghelm.mdl"]                  = {Vector(0,0,-5)  , Angle(0,20,0)    , 20},
-	["models/daftpunk/shelm.mdl"]                  = {Vector(0,0,-5)  , Angle(0,20,0)    , 20},
-	["models/piratehat/piratehat2.mdl"]            = {Vector(0,0,-2)  , Angle(0,180,0)   , 30},
-	["models/cloud/kn_santahat.mdl"]               = {Vector(0,0,-2)  , Angle(0,180,0)   , 30},
-	["models/sinful/angel_wings.mdl"]              = {Vector(30,0,0)  , Angle(0,10,0)    , 125},
-
-	--Weapons
-
-	["models/weapons/w_crowbar.mdl"]               = {Vector(0,0,0)   , Angle(45,0,0)    , 50},
-	["models/weapons/lordi/c_sledgehammer.mdl"]    = {Vector(-14,0,-4), Angle(-32,-4,67) , 75},
-
-	["models/weapons/w_pistol.mdl"]                = {Vector(-1,0,1)  , Angle(0,180,0)   , 20},
-	["models/weapons/w_pist_fiveseven.mdl"]        = {Vector(-3,0,-4) , Angle(0,0,0)     , 20},
-	["models/weapons/w_357.mdl"]                   = {Vector(-8,0,0)  , Angle(0,0,0)     , 25},
-	["models/weapons/w_pist_deagle.mdl"]           = {Vector(-4,0,-4) , Angle(0,0,0)     , 22},
-	["models/goldengun.mdl"]                       = {Vector(-20,0,0) , Angle(0,0,0)     , 20},
-
-	["models/weapons/w_smg_tmp.mdl"]               = {Vector(-8,0,-2) , Angle(0,0,0)     , 50},
-	["models/weapons/w_smg1.mdl"]                  = {Vector(0,0,0)   , Angle(0,0,0)     , 35},
-	["models/weapons/w_rif_m4a1.mdl"]              = {Vector(4,0,-5)  , Angle(-40,0,0)   , 50},
-	["models/weapons/w_irifle.mdl"]                = {Vector(-9,0,0)  , Angle(30,0,0)    , 60},
-	["models/weapons/w_rif_ak47.mdl"]              = {Vector(0,0,-8)  , Angle(-40,0,0)   , 50},
-	["models/weapons/w_rif_galil.mdl"]             = {Vector(-0,0,-5) , Angle(-40,0,0)   , 50},
-	["models/weapons/w_rif_famas.mdl"]             = {Vector(5,0,-5)  , Angle(-45,0,0)   , 43},
-
-	["models/weapons/weapon_shotgun.mdl"]          = {Vector(0,0,0)   , Angle(0,0,0)     , 80},
-	["models/weapons/w_shotgun.mdl"]               = {Vector(-3,0,0)  , Angle(30,0,0)    , 50},
-
-	["models/weapons/w_snip_awp.mdl"]              = {Vector(0,0,-2)  , Angle(-45,-45,0) , 80},
-	["models/weapons/w_dragunov_svu.mdl"]          = {Vector(-5,0,-8) , Angle(-40,0,0)   , 50},
-	["models/weapons/w_crossbow.mdl"]              = {Vector(-10,0,0) , Angle(0,0,-30)   , 70},
-	["models/weapons/w_snip_scoub.mdl"]            = {Vector(-10,0,0) , Angle(0,0,0)     , 60},
-
-	["models/weapons/w_mach_m249para.mdl"]         = {Vector(-6,0,-5) , Angle(30,0,0)    , 60},
-	["models/weapons/w_grenade.mdl"]               = {Vector(0,0,-5)  , Angle(0,0,0)     , 30},
-	["models/weapons/b_bazooka_f.mdl"]             = {Vector(0,0,-3)  , Angle(0,0,-20)   , 60},
-	["models/weapons/tfa_nmrih/w_exp_molotov.mdl"] = {Vector(4,0,-3)  , Angle(45,140,0)  , 20},
-	["models/weapons/tfa_nmrih/w_exp_tnt.mdl"]     = {Vector(0,0,0)   , Angle(-90,90,0)  , 30},
-
-	["models/weapons/w_c4.mdl"]                    = {Vector(0,0,0)   , Angle(40,90,0)   , 20},
-	["models/weapons/w_fire_extinguisher.mdl"]     = {Vector(0,0,-18) , Angle(0,0,0)     , 70},
-	["models/freeman/harpoongun.mdl"]              = {Vector(0,0,0)   , Angle(40,0,0)    , 50},
-	["models/weapons/w_bugbait.mdl"]               = {Vector(0,0,0)   , Angle(0,0,0)     , 15},
-	["models/weapons/w_stunbaton.mdl"]             = {Vector(0,0,0)   , Angle(45,0,0)    , 40},
-
-}
+
+--[[
+Floattranslation
+]]
+local floaty = {}
+floaty["-3"] = "Extremely Bad"
+floaty["-2"] = "Very Bad"
+floaty["-1"] = "Bad"
+floaty["0"]  = "Average"
+floaty["1"]  = "Good"
+function TranslateFloat(float)
+	return floaty[tostring(float)]
+end
+
+--[[
+Weapon left and rightclick underwater allowance
+]]
+AllowLeftClick = {
+	["weapon_crowbarfm"]  = true,
+	["weapon_sledgehammer"]  = true,
+	["weapon_pistolfm"]  = true,
+	["weapon_stunstick"]  = true,
+	["weapon_harpoonfm"]  = true,
+	["weapon_crossbow"]   = true,
+	["weapon_physgun"]    = true,
+	["weapon_physcannon"] = true,
+	["gmod_tool"]         = true,
+	["weapon_crowbar"]    = true
+}
+AllowRightClick = {
+	["weapon_harpoonfm"]   = true,
+	["weapon_physgun"]     = true,
+	["weapon_sledgehammer"]  = true,
+	["weapon_physcannon"]  = true,
+	["gmod_tool"]          = true,
+	["weapon_awpfm"]       = true,
+	["weapon_svufm"]       = true,
+	["weapon_ak47fm"]      = true,
+	["weapon_m4fm"]        = true,
+	["weapon_tmpfm"]       = true,
+	["weapon_galilfm"]     = true,
+	["weapon_parafm"]      = true,
+	["weapon_fivesevenfm"] = true,
+	["weapon_deaglefm"]    = true,
+	["weapon_crossbow"]    = true,
+	["weapon_famasfm"]     = true
+}
+
+--[[
+Festives
+]]
+Gift_PopupTitle = "Yay!"
+Gift_PopupMessage = "You've received a gift. You were given:"
+
+--[[
+Icon adjusting
+]]
+IconCoords = {
+	--Store content
+	["models/mymodels/fezhat.mdl"]                 = {Vector(0,0,0)   , Angle(0,-40,0)   , 20    , Color(255,0,0)},
+	["models/daftpunk/ghelm.mdl"]                  = {Vector(0,0,-5)  , Angle(0,20,0)    , 20},
+	["models/daftpunk/shelm.mdl"]                  = {Vector(0,0,-5)  , Angle(0,20,0)    , 20},
+	["models/piratehat/piratehat2.mdl"]            = {Vector(0,0,-2)  , Angle(0,180,0)   , 30},
+	["models/cloud/kn_santahat.mdl"]               = {Vector(0,0,-2)  , Angle(0,180,0)   , 30},
+	["models/sinful/angel_wings.mdl"]              = {Vector(30,0,0)  , Angle(0,10,0)    , 125},
+
+	--Weapons
+	["models/weapons/w_crowbar.mdl"]               = {Vector(0,0,0)   , Angle(45,0,0)    , 50},
+
+	["models/weapons/w_pistol.mdl"]                = {Vector(-1,0,1)  , Angle(0,180,0)   , 20},
+	["models/weapons/w_pist_fiveseven.mdl"]        = {Vector(-3,0,-4) , Angle(0,0,0)     , 20},
+	["models/weapons/w_357.mdl"]                   = {Vector(-8,0,0)  , Angle(0,0,0)     , 25},
+	["models/weapons/w_pist_deagle.mdl"]           = {Vector(-4,0,-4) , Angle(0,0,0)     , 22},
+	["models/goldengun.mdl"]                       = {Vector(-20,0,0) , Angle(0,0,0)     , 20},
+
+	["models/weapons/w_smg_tmp.mdl"]               = {Vector(-8,0,-2) , Angle(0,0,0)     , 50},
+	["models/weapons/w_smg1.mdl"]                  = {Vector(0,0,0)   , Angle(0,0,0)     , 35},
+
+	["models/weapons/w_rif_m4a1.mdl"]              = {Vector(4,0,-5)  , Angle(-40,0,0)   , 50},
+	["models/weapons/w_shotgun.mdl"]               = {Vector(-3,0,0)  , Angle(30,0,0)    , 50},
+	["models/weapons/w_irifle.mdl"]                = {Vector(-9,0,0)  , Angle(30,0,0)    , 60},
+	["models/weapons/w_rif_ak47.mdl"]              = {Vector(0,0,-8)  , Angle(-40,0,0)   , 50},
+	["models/weapons/w_rif_galil.mdl"]             = {Vector(-0,0,-5) , Angle(-40,0,0)   , 50},
+	["models/weapons/w_rif_famas.mdl"]             = {Vector(5,0,-5)  , Angle(-45,0,0)   , 43},
+
+	["models/weapons/weapon_shotgun.mdl"]          = {Vector(0,0,0)   , Angle(0,0,0)     , 80},
+	["models/weapons/w_snip_awp.mdl"]              = {Vector(0,0,-2)  , Angle(-45,-45,0) , 80},
+	["models/weapons/w_dragunov_svu.mdl"]          = {Vector(-5,0,-8) , Angle(-40,0,0)   , 50},
+	["models/weapons/w_crossbow.mdl"]              = {Vector(-10,0,0) , Angle(0,0,-30)   , 70},
+	["models/weapons/w_snip_scoub.mdl"]            = {Vector(-10,0,0) , Angle(0,0,0)     , 60},
+
+	["models/weapons/w_mach_m249para.mdl"]         = {Vector(-6,0,-5) , Angle(30,0,0)    , 60},
+	["models/weapons/w_grenade.mdl"]               = {Vector(0,0,-5)  , Angle(0,0,0)     , 30},
+	["models/weapons/b_bazooka_f.mdl"]             = {Vector(0,0,-3)  , Angle(0,0,-20)   , 60},
+	["models/weapons/tfa_nmrih/w_exp_molotov.mdl"] = {Vector(4,0,-3)  , Angle(45,140,0)  , 20},
+	["models/weapons/tfa_nmrih/w_exp_tnt.mdl"]     = {Vector(0,0,0)   , Angle(-90,90,0)  , 30},
+	["models/weapons/lordi/c_sledgehammer.mdl"]    = {Vector(-14,0,-4), Angle(-32,-4,67) , 75},
+
+	["models/weapons/w_c4.mdl"]                    = {Vector(0,0,0)   , Angle(40,90,0)   , 20},
+	["models/weapons/w_fire_extinguisher.mdl"]     = {Vector(0,0,-18) , Angle(0,0,0)     , 70},
+	["models/freeman/harpoongun.mdl"]              = {Vector(0,0,0)   , Angle(40,0,0)    , 50},
+	["models/weapons/w_bugbait.mdl"]               = {Vector(0,0,0)   , Angle(0,0,0)     , 15},
+	["models/weapons/w_stunbaton.mdl"]             = {Vector(0,0,0)   , Angle(45,0,0)    , 40}
+}
--- gamemodes/flood/gamemode/config.lua
+++ gamemodes/flood/gamemode/config.lua
@@ -1,206 +1,146 @@
-BuildT   = 180 -- Time to building boats
-PrepareT = 10  -- Time for getting on boats
-FloodT   = 16  -- Time for water to fill
-FightT   = 180 -- Time to Fight
-ReflectT = 20  -- Time after fight
-
-TeamMembersPerPlayers = 0.15 -- How many max teammembers is based on players. If 1, member limit would be 6 if serverpop was 6. If 0.5, member limit would be 3 if serverpop was 6. This is rounded up.
-
--- Basically a list of all tools to be seen in the qmenu.
-
-VisibleTools = {
-
-	"floodflag",
-	"thruster",
-	"weld",
-	"smartwelder",
-	"remover",
-	"floodcannon",
-	"floodmotor",
-	"paint",
-	"balloon",
-	"rope",
-	"camera",
-	"trails",
-	"colour",
-	"light",
-	"material",
-	"specialprops",
-
-}
-
--- What tools should be useable for everyone for everyround.
-
-ToolsWhitelist = {
-
-	"floodflag",
-	"weld",
-	"smartwelder",
-	"remover",
-	"floodmotor",
-	"specialprops",
-
-}
-
-JumpUpCooldown = 3  -- How many seconds until you can attempt to jump up again
-
-MaxThrusterForce = 100
-ThrusterPrice = 150
-
-CannonsPerPlayer = 1
-CannonCost       = 0
-
-MotorsPerPlayer = 1
-
-FlagsPerPlayer = 2
-
-DeathRagLifetime = 10
-
-BugbaitRadius             = 300  -- Area of effect
-BugbaitDPS                = 1    -- Damage per second it does to players
-BugbaitTime               = 10   -- How long the gascloud stays for
-BugbaitOnlyExplodeInWater = true -- Makes the bugbait only explode when it hits water, if it hits a prop, it dies after a few seconds.
-BugbaitFriendlyFire       = true -- Can the gascloud harm teammates/yourself?
-
---[[
-
-Hatschat Config
-
-]]
-
---Predefined Emoticons
-
-HatsChat_Emoticons = {
-
-	{"???",        Material("icon32/chat/confused.png")},
-	{":rolleyes:", Material("icon32/chat/rolleyes.png")},
-	{":cool:",     Material("icon32/chat/cool.png")},
-	{">:(",        Material("icon32/chat/mad.png")},
-	{":)",         Material("icon16/emoticon_smile.png")},
-	{":(",         Material("icon16/emoticon_unhappy.png")},
-	{":p",         Material("icon16/emoticon_tongue.png")},
-	{">:D",        Material("icon16/emoticon_evilgrin.png")},
-	{":D",         Material("icon16/emoticon_grin.png")},
-	{":o",         Material("icon16/emoticon_surprised.png")},
-	{";)",         Material("icon16/emoticon_wink.png")},
-	{":3",         Material("icon16/emoticon_waii.png")},
-	{"^^",         Material("icon16/emoticon_happy.png")},
-	{"<3",         Material("icon16/heart.png")},
-	{":gmod:",     Material("games/16/garrysmod.png")},
-	{":troll:",    Material("icon32/chat/trollface.png")},
-	{"Kappa",      Material("icon32/chat/kappa.png")},
-	{"Peka",       Material("icon32/chat/peka.png")},
-
-}
-
---Line icons
-
-HatsChat_LineIcons = {
-
-}
-
---[[
-
-Token stuff
-
-]]
-
-function TokenToUSDEquation( tokens )
-	if tokens < 0 then return 0 end
-
-	return math.Round(math.pow(tokens, 0.75) / 4, 2)
-end
-
-
-
-function CashToTokenEquation( cash )
-	return cash * 1000
-end
-
-
-
-function TokenToCashEquation( tokens )
-	return tokens * 2000
-end
-
-MINTOKENPURCHASE = 5 --Smallest amount of tokens you can purchase in a paypal purchase.
-
-GROUPTOKENREWARD = 10
-
---[[
-
-VIP stuff
-
-]]
-
---AddVIPOnlyTool(toolname, viptier)
-
-AddVIPOnlyTool("floodcannon" , 0)
-AddVIPOnlyTool("paint"       , 1)
-AddVIPOnlyTool("wheel"       , 1)
-AddVIPOnlyTool("balloon"     , 1)
-AddVIPOnlyTool("rope"        , 1)
-AddVIPOnlyTool("camera"      , 1)
-AddVIPOnlyTool("trails"      , 1)
-AddVIPOnlyTool("colour"      , 1)
-AddVIPOnlyTool("light"       , 2)
-AddVIPOnlyTool("thruster"    , 2)
-AddVIPOnlyTool("material"    , 3)
-
---What tier should build-noclip be available for? (0 to let everyone use it)
-
-VIPNoclip = 2
-
---VipTiers, first one in list will be tier 1, fourth will be tier 4.
-
---Supports up to 16 tiers.
-
-AddVIPTier("Bronze"      , Color(150,90,56))
-AddVIPTier("Silver"      , Color(166,166,166))
-AddVIPTier("Gold"        , Color(255,215,0))
-AddVIPTier("Platinum"    , Color(122,148,162))
-AddVIPTier("JR Mod"      , Color(26, 188, 156))
-AddVIPTier("Moderator"   , Color(52, 152, 219))
-AddVIPTier("SR Mod"      , Color(230, 126, 34))
-AddVIPTier("Admin"       , Color(220, 20, 60))
-AddVIPTier("Super Admin" , Color(26, 188, 156))
-AddVIPTier("Founder"     , Color(148, 204, 75))
-
--- Admin fallback model
-
-AdminModel = "models/player/11thdoctor/thedoctor.mdl"
-
---[[
-
-WeaponStuff
-
-]]
-
---Adds health to specific entities, such as cannons or thrusters
-
---AddEntityHealth(entname, health)
-
-AddEntityHealth("fm_cannon", 150)
-AddEntityHealth("fm_boatmotor", 300)
-AddEntityHealth("fm_flag", 150)
-AddEntityHealth("gmod_thruster", 50)
-AddEntityHealth("gmod_light_fm", 10)
-
---[[
-
-WEAPONS
-
-]]
-
-hook.Add("OnEntityCreated", "GiveDamageToSpecialWeapons", function(ent)
-	if ent:GetClass() == "grenade_ar2" then
-
-		ent.Damage = 15
-
-	elseif ent:GetClass() == "crossbow_bolt" and GetWeaponData("weapon_crossbow") then
-
-		ent.Damage = GetWeaponData("weapon_crossbow").damage
-		
-	end
-end)
-
+BuildT   = 180 -- Time to building boats
+PrepareT = 10  -- Time for getting on boats
+FloodT   = 16  -- Time for water to fill
+FightT   = 180 -- Time to Fight
+ReflectT = 20  -- Time after fight
+
+TeamMembersPerPlayers = 0.15 -- How many max teammembers is based on players. If 1, member limit would be 6 if serverpop was 6. If 0.5, member limit would be 3 if serverpop was 6. This is rounded up.
+
+
+-- Basically a list of all tools to be seen in the qmenu.
+VisibleTools = {
+	"floodflag", "thruster", "weld", "smartwelder", "remover", "floodcannon", "floodmotor", "paint", "balloon", "rope", "camera", "trails", "colour", "light", "material", "specialprops"
+}
+
+-- What tools should be useable for everyone for everyround.
+ToolsWhitelist = {
+	"floodflag", "weld", "smartwelder", "remover", "floodmotor", "specialprops"
+}
+
+JumpUpCooldown = 3  -- How many seconds until you can attempt to jump up again
+
+MaxThrusterForce = 100
+ThrusterPrice = 150
+
+CannonsPerPlayer = 1
+CannonCost       = 0
+
+MotorsPerPlayer = 1
+
+FlagsPerPlayer = 2
+
+DeathRagLifetime = 10
+
+BugbaitRadius             = 300  -- Area of effect
+BugbaitDPS                = 1    -- Damage per second it does to players
+BugbaitTime               = 10   -- How long the gascloud stays for
+BugbaitOnlyExplodeInWater = true -- Makes the bugbait only explode when it hits water, if it hits a prop, it dies after a few seconds.
+BugbaitFriendlyFire       = true -- Can the gascloud harm teammates/yourself?
+
+--[[
+Hatschat Config
+]]
+--Predefined Emoticons
+HatsChat_Emoticons = {
+	{"???",        Material("icon32/chat/confused.png")},
+	{":rolleyes:", Material("icon32/chat/rolleyes.png")},
+	{":cool:",     Material("icon32/chat/cool.png")},
+	{">:(",        Material("icon32/chat/mad.png")},
+	{":)",         Material("icon16/emoticon_smile.png")},
+	{":(",         Material("icon16/emoticon_unhappy.png")},
+	{":p",         Material("icon16/emoticon_tongue.png")},
+	{">:D",        Material("icon16/emoticon_evilgrin.png")},
+	{":D",         Material("icon16/emoticon_grin.png")},
+	{":o",         Material("icon16/emoticon_surprised.png")},
+	{";)",         Material("icon16/emoticon_wink.png")},
+	{":3",         Material("icon16/emoticon_waii.png")},
+	{"^^",         Material("icon16/emoticon_happy.png")},
+	{"<3",         Material("icon16/heart.png")},
+	{":gmod:",     Material("games/16/garrysmod.png")},
+	{":troll:",    Material("icon32/chat/trollface.png")},
+	{"Kappa",      Material("icon32/chat/kappa.png")},
+	{"Peka",       Material("icon32/chat/peka.png")}
+}
+
+--Line icons
+HatsChat_LineIcons = {
+}
+
+
+--[[
+Token stuff
+]]
+function TokenToUSDEquation( tokens )
+	if tokens < 0 then return 0 end
+
+	return math.Round(math.pow(tokens, 0.75) / 4, 2)
+end
+
+function CashToTokenEquation( cash )
+	return cash * 1000
+end
+
+function TokenToCashEquation( tokens )
+	return tokens * 2000
+end
+
+MINTOKENPURCHASE = 5 --Smallest amount of tokens you can purchase in a paypal purchase.
+
+GROUPTOKENREWARD = 10
+
+--[[
+VIP stuff
+]]
+--AddVIPOnlyTool(toolname, viptier)
+AddVIPOnlyTool("floodcannon" , 0)
+AddVIPOnlyTool("paint"       , 1)
+AddVIPOnlyTool("wheel"       , 1)
+AddVIPOnlyTool("balloon"     , 1)
+AddVIPOnlyTool("rope"        , 1)
+AddVIPOnlyTool("camera"      , 1)
+AddVIPOnlyTool("trails"      , 1)
+AddVIPOnlyTool("colour"      , 1)
+AddVIPOnlyTool("light"       , 2)
+AddVIPOnlyTool("thruster"    , 2)
+AddVIPOnlyTool("material"    , 3)
+
+--What tier should build-noclip be available for? (0 to let everyone use it)
+VIPNoclip = 2
+
+--VipTiers, first one in list will be tier 1, fourth will be tier 4.
+--Supports up to 16 tiers.
+AddVIPTier("Bronze"      , Color(150,90,56))
+AddVIPTier("Silver"      , Color(166,166,166))
+AddVIPTier("Gold"        , Color(255,215,0))
+AddVIPTier("Platinum"    , Color(122,148,162))
+AddVIPTier("JR Mod"      , Color(26, 188, 156))
+AddVIPTier("Moderator"   , Color(52, 152, 219))
+AddVIPTier("SR Mod"      , Color(230, 126, 34))
+AddVIPTier("Admin"       , Color(220, 20, 60))
+AddVIPTier("Super Admin" , Color(26, 188, 156))
+AddVIPTier("Founder"     , Color(148, 204, 75))
+
+-- Admin fallback model
+AdminModel = "models/player/11thdoctor/thedoctor.mdl"
+
+--[[
+WeaponStuff
+]]
+--Adds health to specific entities, such as cannons or thrusters
+--AddEntityHealth(entname, health)
+AddEntityHealth("fm_cannon", 150)
+AddEntityHealth("fm_boatmotor", 300)
+AddEntityHealth("fm_flag", 150)
+AddEntityHealth("gmod_thruster", 50)
+AddEntityHealth("gmod_light_fm", 10)
+
+--[[
+WEAPONS
+]]
+hook.Add("OnEntityCreated", "GiveDamageToSpecialWeapons", function(ent)
+	if ent:GetClass() == "grenade_ar2" then
+		ent.Damage = 15
+	elseif ent:GetClass() == "crossbow_bolt" and GetWeaponData("weapon_crossbow") then
+		ent.Damage = GetWeaponData("weapon_crossbow").damage
+	end
+end)
